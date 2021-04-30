ce7056af5ada0c77735bbe0d8f12e828acd2c1a4
Tim Friedrich <tim.friedrich-2@mni.thm.de>
2021-04-30 16:48:11 +0200

    Landingpage in Components aufgeteilt und erste Routen erstellt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleContentComponent } from './example-content/example-content.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { WalletsComponent } from './wallets/wallets.component';
+import { AddressesComponent } from './addresses/addresses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleContentComponent,
+    NavbarComponent,
+    TransactionsComponent,
+    WalletsComponent,
+    AddressesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/example-content/example-content.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExampleContentComponent } from './example-content.component';
+
+describe('ExampleContentComponent', () => {
+  let component: ExampleContentComponent;
+  let fixture: ComponentFixture<ExampleContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ExampleContentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExampleContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
