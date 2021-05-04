af477318108f274e3c9f49e79fc0f785f9c6cdb4
Connor Landes <connor.landes@mni.thm.de>
2021-05-04 16:28:38 +0200

    Added web3.js and Metamask connection

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import Web3 from 'web3';
+declare var window: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  private web3: any;
+  ethEnabled = async () => {
+    if (window.ethereum) {
+      await window.ethereum.send('eth_requestAccounts');
+      window.web3 = new Web3(window.ethereum);
+      return true;
+    }
+    return false;
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getMetaMask(): void{
+    if (this.ethEnabled()){
+      console.log('Test');
+    }
   }
 
 }
