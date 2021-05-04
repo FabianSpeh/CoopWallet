244fc08f8ca6d11c4c63a9068c52de6c5f95fe53
Connor Landes <connor.landes@mni.thm.de>
2021-05-04 16:32:22 +0200

    Added web3.js and Metamask connection

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -23,7 +23,7 @@ export class NavbarComponent implements OnInit {
 
   getMetaMask(): void{
     if (this.ethEnabled()){
-      console.log('Test');
+      console.log('Connect to Metamask');
     }
   }
 
