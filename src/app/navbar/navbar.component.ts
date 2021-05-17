187be779ca0850fa952a38b25493da8eea65eb3f
Connor Landes <connor.landes@mni.thm.de>
2021-05-17 15:41:38 +0200

    Added Comments to navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,34 +11,57 @@ declare var window: any;
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit, OnDestroy {
+  /**
+   * Class provides methods which are used as a help for the creation of the navbar.
+   *
+   * @param network - saves the current networkname.
+   * @param accountIndex - has the current selected account as an index of a account array
+   * @param ethereumEnabled - saves if a window.ethereum Provider is installed in the browser
+   * @param dataGot - is true if the window.ethereum Provider answered.
+   * @param interval - used to setup an Interval which calls the update Method after updateTime time
+   * @param updateTime - time how often the update Method should be called
+   * @param web3js - web3
+   */
   network: any;
   accountIndex = 0;
-  etherumEnabled: boolean;
+  ethereumEnabled: boolean;
   dataGot: boolean;
   interval: any;
   updateTime = 600000;
   web3js: any;
 
+  /**
+   * Constructor to set up the services and give ethereumEnabled and data got initial false values
+   * @param service - Service which called after a browser refresh and check if there is already a Connection to a provider established
+   * @param userService - Service which provides all relevant Data from the User Wallet.
+   */
   constructor(private service: BrowserRefreshService, public userService: UserWalletDataService) {
-    this.etherumEnabled = false;
+    this.ethereumEnabled = false;
     this.dataGot = false;
   }
 
-  // tslint:disable-next-line:typedef
-  async checkData(){
-    this.etherumEnabled = await this.service.checkEtherumConection();
+  /**
+   * Calls BrowserRefreshService to check if there is already a Connected provider to the Site
+   */
+  async checkData(): Promise<void>{
+    this.ethereumEnabled = await this.service.checkEtherumConection();
     this.network = this.service.network;
   }
 
+  /**
+   * First check if there already a connection and if there is gets the Data from the User Wallet, then set up various Methods to refresh
+   * User Data
+   */
   async ngOnInit(): Promise<void> {
     await this.checkData();
-    if (this.etherumEnabled){
+    if (this.ethereumEnabled){
       await this.connectMetaMask();
     }
+    /** Set up Interval to call the Update Method every updateTime time */
     this.interval = setInterval(() => this.update(), this.updateTime);
     if (window.ethereum) {
       this.web3js = new Web3(window.ethereum);
-      // tslint:disable-next-line:only-arrow-functions typedef
+      // Old Method of checking for new User Data
       /*this.web3js.eth.subscribe('newBlockHeaders', (error: any, result: any) => {
         if (!error) {
           console.log("New BlockHeader");
@@ -49,6 +72,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
         console.error(error);
       });*/
 
+      /** Setting up of Various Event Listener to act own User Wallet Data Changes */
       window.ethereum.on('accountsChanged', () => {
         console.log('accountChanged');
         this.accountIndex = 0;
@@ -72,14 +96,21 @@ export class NavbarComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Update Method to Update all the Data from the User Wallet
+   */
  async update(): Promise<void>{
     console.log('Update Started');
     await this.checkData();
-    if (this.etherumEnabled){
+    if (this.ethereumEnabled){
       await this.connectMetaMask();
     }
   }
 
+
+  /**
+   * Calls the User-Wallet-Data Service to get all the Data from the User Wallet
+   */
   async connectMetaMask(): Promise<void> {
     this.dataGot = await this.userService.getData(this.accountIndex);
     if (this.dataGot) {
@@ -87,6 +118,11 @@ export class NavbarComponent implements OnInit, OnDestroy {
     }
   }
 
+
+  /**
+   * Help Method for Account Switching on the Site
+   * @param index - Index of the Account to which the User switches on the Site
+   */
   async accountSwitch(index: number): Promise<void> {
     this.accountIndex = index;
     this.dataGot = await this.userService.getData(this.accountIndex);
@@ -95,6 +131,9 @@ export class NavbarComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Cleanup of the Interval and subscriptions.
+   */
   ngOnDestroy(): void {
     clearInterval(this.interval);
     this.web3js.eth.clearSubscriptions();
