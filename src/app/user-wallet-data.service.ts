f33db171fa2eb52a84d511f54776abeda1c860fb
Connor Landes <connor.landes@mni.thm.de>
2021-05-17 16:01:47 +0200

    Added Comments to user-wallet-data.service.ts

--- src/app/user-wallet-data.service.ts
+++ src/app/user-wallet-data.service.ts
@@ -7,7 +7,18 @@ declare var window: any;
   providedIn: 'root'
 })
 export class UserWalletDataService {
-
+  /**
+   * Service to get all User Wallet Data
+   * @param balance - Current User Wallet Balance
+   * @param accounts - Array of all User Wallet Accounts
+   * @param web3js - Web3
+   * @param network - Current User Wallet Network
+   * @param providerOnline - boolean which saves the status of the provider
+   * @param chainId - Current Chain Id of the User Wallet
+   * @param nonce - Current Nonce of the User Wallet
+   * @param accountIndex - Current index of the Selected account on the Site of the accounts array.
+   * @param dataGot - Save if the Data has been extracted from the User Wallet
+   */
   balance: number;
   accounts: any;
   web3js: any;
@@ -20,6 +31,9 @@ export class UserWalletDataService {
   accountIndex = 0;
   dataGot: boolean;
 
+  /**
+   * Set up all initial data to be empty or 0
+   */
   constructor() {
     this.balance = 0;
     this.dataGot = false;
@@ -29,6 +43,10 @@ export class UserWalletDataService {
     this.providerOnline = false;
   }
 
+  /**
+   * Extract all relevant data from the User Wallet
+   * @param accountIndex - Index of the Select Account, of the on Site select User Wallet
+   */
   public async getData(accountIndex: number): Promise<boolean>{
     this.accountIndex = accountIndex;
     if (window.ethereum) {
