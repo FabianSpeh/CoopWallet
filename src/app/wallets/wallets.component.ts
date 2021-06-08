1d7b0818247ecf12f60b6bf50b3c0f0749555be4
Tim Friedrich <tim.friedrich-2@mni.thm.de>
2021-06-08 22:59:35 +0200

    Exchanged await chain in readCookies() for a function that returns a Wallet Promise

--- src/app/wallets/wallets.component.ts
+++ src/app/wallets/wallets.component.ts
@@ -2,6 +2,18 @@ import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ClipboardService} from 'ngx-clipboard';
 import {MultisigWalletDataService} from '../services/multisig-wallet-data.service';
 
+
+
+interface Wallet {
+  name: string;
+  address: string;
+  balance: string;
+  confirmations: string;
+  owners: string;
+  pending: string;
+  network: string;
+}
+
 @Component({
   selector: 'app-wallets',
   templateUrl: './wallets.component.html',
@@ -64,29 +76,38 @@ export class WalletsComponent implements OnInit {
    * Loads stored Wallets into WalletsData
    */
   async readCookies(): Promise<void> {
-    // const wallets = JSON.parse(this.cookieService.get('Wallets'));
     if (localStorage.getItem('Wallets') == null){
-    return;
+      return;
     }
     const wallets = JSON.parse(localStorage.getItem('Wallets') || '{}');
     for (let i = 0; i < wallets.name.length; i++) {
-      let address = wallets.address[i];
-      await this.walletService.getBalance(address);
-      await this.walletService.getNumberOfConfirmations(address);
-      await this.walletService.getNumberOfOwners(address);
-      await this.walletService.getTransActionCount(address);
-      await this.walletService.getNetwork(address);
-      this.walletsData.push({
-        name: wallets.name[i],
-        address: wallets.address[i],
-        balance: this.walletService.balance.toString(),
-        confirmations: this.walletService.numberOfConfirmations.toString(),
-        owners: this.walletService.ownerListNumber.toString(),
-        pending: this.walletService.pendingNonce.toString(),
-        network: this.walletService.network.toString()
-      });
+      this.getWalletJSON(wallets.name[i], wallets.address[i]).then((res) => { this.walletsData.push(res); });
     }
     this.change.detectChanges();
+  }
+
+  /**
+   * Returns a Wallet-Promise
+   * @param name: the local name of the wallet
+   * @param address: the address used to get the wallet information
+   */
+  getWalletJSON(name: string, address: string): Promise<Wallet> {
+    const wallet: Wallet = {
+      name, address, balance: '', confirmations: '', owners: '', pending: '', network: ''
+    };
+    return new Promise<Wallet>((resolve, reject) => {
+      this.walletService.getBalance(address)
+        .then( () => wallet.balance = this.walletService.balance.toString());
+      this.walletService.getNumberOfConfirmations(address)
+        .then( () => wallet.confirmations = this.walletService.numberOfConfirmations.toString());
+      this.walletService.getNumberOfOwners(address)
+        .then( () => wallet.owners = this.walletService.ownerListNumber.toString());
+      this.walletService.getTransActionCount(address)
+        .then( () => wallet.pending = this.walletService.pendingNonce.toString());
+      this.walletService.getNetwork(address)
+        .then( () => wallet.network = this.walletService.network.toString());
+      resolve(wallet);
+    });
   }
 
   async ngOnInit(): Promise<void> {
